e0cf6a3946fc23005941b1723749b532515cca4b
CKanja <c.kanja@alustudent.com>
2021-09-14 23:10:16 +0200

    "Code that tells the year that a user will be 100 years old"

---
+++ 100years.py
@@ -0,0 +1,5 @@
+name = input("What's your lovely name? ")
+age = int(input("How old are you? "))
+
+no_years = str((100-age)+2021)
+print("Hello " + name + " you will be 100 years old in " + no_years)
